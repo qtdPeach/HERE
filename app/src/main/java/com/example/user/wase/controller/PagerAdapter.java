c101b50f8a55538f5bb680d7b3e6468d03c3fe47
BaekHorang <ymbaek@se.kaist.ac.kr>
2016-04-18 08:04:11 +0900

    - Refactoring package structure - Add fragments for tabs in MainActivity

---
+++ app/src/main/java/com/example/user/wase/controller/PagerAdapter.java
@@ -0,0 +1,50 @@
+package com.example.user.wase.controller;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.example.user.wase.view.fragment.DeviceListFragment;
+import com.example.user.wase.view.fragment.ExerciseFragment;
+import com.example.user.wase.view.fragment.MyRecordsFragment;
+
+/**
+ * Created by ymbae on 2016-04-18.
+ */
+public class PagerAdapter extends FragmentStatePagerAdapter {
+
+    int mNumOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                DeviceListFragment tab1 = new DeviceListFragment();
+                return tab1;
+            case 1:
+                ExerciseFragment tab2 = new ExerciseFragment();
+                return tab2;
+            case 2:
+                MyRecordsFragment tab3 = new MyRecordsFragment();
+                return tab3;
+            default:
+                return null;
+
+        }
+
+
+
+        //return null;
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
---
+++ app/src/main/java/com/example/user/wase/view/fragment/ExerciseFragment.java
@@ -0,0 +1,23 @@
+package com.example.user.wase.view.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.user.wase.R;
+
+/**
+ * Created by ymbae on 2016-04-18.
+ */
+public class ExerciseFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_exercise, container, false);
+        //return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
