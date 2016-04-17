f9284d86af70ffe4b0888c13269e3826a054a0b3
SunggeunAhn <topmaze@kaist.ac.kr>
2016-04-18 03:32:10 +0900

    available!

--- app/src/main/java/com/example/user/wase/Main.java
+++ app/src/main/java/com/example/user/wase/Main.java
@@ -22,7 +22,7 @@ public class Main extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        System.out.print("Fucking studio");
+        System.out.print("GOOD");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
