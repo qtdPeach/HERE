bc1214f5ad8eb81cb10571ad4f1a2a40991bc78e
BaekHorang <ymbaek@se.kaist.ac.kr>
2016-04-18 03:56:39 +0900

    - Refactor -- Model, View, Controller

--- app/src/main/java/com/example/user/wase/View/MainActivity.java
+++ app/src/main/java/com/example/user/wase/View/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.user.wase;
+package com.example.user.wase.View;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.user.wase.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
