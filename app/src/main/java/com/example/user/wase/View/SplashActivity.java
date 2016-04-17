46c75bc3e4991f9e6f3b9643f7f74eac9a159a55
BaekHorang <ymbaek@se.kaist.ac.kr>
2016-04-18 04:00:10 +0900

    - Add SplashActivity.java - Modify AndroidManifest.xml -- Add SplashActivity as a launcher activity

---
+++ app/src/main/java/com/example/user/wase/View/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.example.user.wase.View;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ymbae on 2016-04-18.
+ */
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
