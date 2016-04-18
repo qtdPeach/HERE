9f039443d5d986381304cb8e1c9b171b30d53609
BaekHorang <ymbaek@se.kaist.ac.kr>
2016-04-18 09:36:41 +0900

    - Add activity files

---
+++ app/src/main/java/com/example/user/wase/view/activity/MyRecordsActivity.java
@@ -0,0 +1,59 @@
+package com.example.user.wase.view.activity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.example.user.wase.R;
+
+/**
+ * Created by ymbaek on 2016-04-18.
+ */
+public class MyRecordsActivity extends AppCompatActivity {
+
+    public static final String TAG = "MyRecordsActivity";
+
+    private Toolbar toolbar;
+
+    //Change
+    int i = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myrecords);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        toolbar.setTitle("내 운동 기록");
+        toolbar.setNavigationIcon(R.mipmap.ic_menu_arrow_back);
+        toolbar.setTitleTextColor(Color.WHITE);
+
+        setSupportActionBar(toolbar);
+
+        final ActionBar actionBar = getSupportActionBar();
+
+        actionBar.setTitle("내 운동 기록");
+        actionBar.setDefaultDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/user/wase/view/activity/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.user.wase.view.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.user.wase.R;
+
+/**
+ * Created by ymbaek on 2016-04-18.
+ */
+public class SplashActivity extends Activity {
+
+    public static final int SPLASH_TIMEOUT_MILLIS = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIMEOUT_MILLIS);
+    }
+}
