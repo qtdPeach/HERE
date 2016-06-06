c2d6b4956ddbcf44f6ac659f6c214a481fbdd9fa
BaekHorang <ymbaek@se.kaist.ac.kr>
2016-06-07 01:19:42 +0900

    - SettingMyInfoActivity의 자잘한 오류 핸들링, 정보 수정 -- 예외처리 (Integer values) -- User ID를 기반으로 데이터베이스 업데이트

--- app/src/main/java/com/example/user/wase/view/activity/SettingMyInfoActivity.java
+++ app/src/main/java/com/example/user/wase/view/activity/SettingMyInfoActivity.java
@@ -121,6 +121,7 @@ public class SettingMyInfoActivity extends AppCompatActivity {
             settingMyInfo_tv_name.setText(myInformation.getUserName());
 
             settingMyInfo_et_id.setText(myInformation.getUserId());
+            settingMyInfo_et_id.setEnabled(false);
             settingMyInfo_et_nick.setText(myInformation.getUserNick());
             settingMyInfo_et_name.setText(myInformation.getUserName());
             settingMyInfo_et_age.setText(String.valueOf(myInformation.getUserAge()));
@@ -153,13 +154,40 @@ public class SettingMyInfoActivity extends AppCompatActivity {
             case R.id.setting_myinfo_btn_save:
                 MyInformation myInfo = new MyInformation();
 
+                //TODO: Integer.parseInt에서 "" 처리
                 String userId = settingMyInfo_et_id.getText().toString();
                 String userNick = settingMyInfo_et_nick.getText().toString();
                 String userName = settingMyInfo_et_name.getText().toString();
-                int userAge = Integer.parseInt(settingMyInfo_et_age.getText().toString());
-                int userSex = Integer.parseInt(settingMyInfo_et_sex.getText().toString());
-                int userHeight = Integer.parseInt(settingMyInfo_et_height.getText().toString());
-                int userWeight = Integer.parseInt(settingMyInfo_et_weight.getText().toString());
+
+                int userAge;
+                int userSex;
+                int userHeight;
+                int userWeight;
+
+                if (!settingMyInfo_et_age.getText().toString().equals("")) {
+                    userAge = Integer.parseInt(settingMyInfo_et_age.getText().toString());
+                } else {
+                    userAge = 20;
+                }
+
+                if (!settingMyInfo_et_sex.getText().toString().equals("")) {
+                    userSex = Integer.parseInt(settingMyInfo_et_sex.getText().toString());
+                } else {
+                    userSex = 1;
+                }
+
+                if (!settingMyInfo_et_height.getText().toString().equals("")) {
+                    userHeight = Integer.parseInt(settingMyInfo_et_height.getText().toString());
+                } else {
+                    userHeight = 170;
+                }
+
+                if (!settingMyInfo_et_weight.getText().toString().equals("")) {
+                    userWeight = Integer.parseInt(settingMyInfo_et_weight.getText().toString());
+                } else {
+                    userWeight = 65;
+                }
+
 
                 myInfo.setUserId(userId);
                 myInfo.setUserNick(userNick);
@@ -170,6 +198,7 @@ public class SettingMyInfoActivity extends AppCompatActivity {
                 myInfo.setUserWeight(userWeight);
                 myInfo.setUserDeviceId(android_id);
 
+                //Update database
                 if (MainActivity.hereDB.getMyInformation() != null) {
                     Log.d(TAG_DB, "[DatabaseTest] User information already exists in DB.");
                     Log.d(TAG_DB, "[DatabaseTest] User information is updated.");
@@ -178,6 +207,13 @@ public class SettingMyInfoActivity extends AppCompatActivity {
                     Log.d(TAG_DB, "[DatabaseTest] User information is added into DB.");
                     MainActivity.hereDB.insertMyInformation(myInfo);
                 }
+
+//                //Update
+//                if (MainActivity.hereDB.getMyInformation() == null) {
+//                    MainActivity.hereDB.insertMyInformation(myInfo);
+//                } else {
+//                    MainActivity.hereDB.updateMyInformation(myInfo);
+//                }
 
                 initWidgetValues();
 
